8b854d0bbb5ddff10a679371fbda0b9f4a30f273
danielrubens <danielrubensdaniel@gmail.com>
2022-11-17 19:31:11 -0300

    connected Express application

--- server/index.js
+++ server/index.js
@@ -2,9 +2,21 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import postRoutes from './routes/posts.js'
 
 const app = express()
 
+app.use('/posts', postRoutes)
 app.use(bodyParser.json({limit: "30mb", entended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", entended: true}))
 app.use(cors())
+
+const CONNECTION_URL = 'mongodb+srv://danielrubens:WWZBkyzklGRxeYyv@cluster0.qy3jhwt.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((e) => console.log(e.message))
+
+
+    // WWZBkyzklGRxeYyv
