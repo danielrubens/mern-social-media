443ccc82553cec39cae9e49e3983dc540859e998
danielrubens <danielrubensdaniel@gmail.com>
2022-11-18 14:17:16 -0300

    solved: Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -3,12 +3,10 @@ import PostMessage from "../models/postMessage.js"
 const getPosts = async (req, res) => {
     try{
         const postMessages = await PostMessage.find()
-        console.log(postMessages)
-        res.status(200).json(postMessages)
+        return res.status(200).json(postMessages)
     }catch(e){
-        res.status(404).json({ message: e.message})
+        return res.status(404).json({ message: e.message})
     }
-    res.send('THIS WORKS')
 }
 
 const createPost = async(req, res) =>{
@@ -18,9 +16,8 @@ const createPost = async(req, res) =>{
         await newPost.save()
         return res.status(201).json(newPost)
     }catch(e){
-        res.status(409).json({ message: e.message })
+        return res.status(409).json({ message: e.message })
     }
-    return res.send('Post Creation')
 }
 
 export {getPosts, createPost}
