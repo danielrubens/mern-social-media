132bd5a2291b022886b64609d2637f41b8a1b306
danielrubens <danielrubensdaniel@gmail.com>
2022-11-18 01:28:09 -0300

    Form created

--- server/index.js
+++ server/index.js
@@ -4,14 +4,13 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import postRoutes from './routes/posts.js'
 
-
 const app = express()
-
-app.use('/posts', postRoutes)
 
 app.use(bodyParser.json({limit: "30mb", entended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", entended: true}))
 app.use(cors())
+
+app.use('/posts', postRoutes)
 
 const CONNECTION_URL = 'mongodb+srv://danielrubens:WWZBkyzklGRxeYyv@cluster0.qy3jhwt.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000
