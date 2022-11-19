e51ed11511c22c1ddd36386e7972e74175e6e844
danielrubens <danielrubensdaniel@gmail.com>
2022-11-19 01:29:33 -0300

    Post styles

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -28,7 +28,7 @@ const Form = () => {
           <TextField name='tags' variant='outlined' label='Tags' fullWidth value={postData.tags}onChange={(e) => setPostData({ ...postData, tags: e.target.value})}></TextField>
           <div className={classes.fileInput}><FileBase type='file' multiple={false} onDone={({ base64 }) => setPostData({...postData, selectedFile: base64})}/></div>
           <Button className={classes.buttonSubmit} variant='contained' color='primary' size='large' type='submit' fullWidth>Submit</Button>
-          <Button variant='contained' color='secondary' size='smaal' onClick={clear} fullWidth>Clear</Button>
+          <Button variant='contained' color='secondary' size='small' onClick={clear} fullWidth>Clear</Button>
       </form>
     </Paper>
   )
