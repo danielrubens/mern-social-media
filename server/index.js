f5d601664dbe547270e90a3dd77796187f721837
danielrubens <danielrubensdaniel@gmail.com>
2022-11-19 21:42:30 -0300

    venv config

--- server/index.js
+++ server/index.js
@@ -3,8 +3,10 @@ import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import postRoutes from './routes/posts.js'
+import dotenv from 'dotenv'
 
 const app = express()
+dotenv.config()
 
 app.use(bodyParser.json({limit: "30mb", entended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", entended: true}))
@@ -12,8 +14,8 @@ app.use(cors())
 
 app.use('/posts', postRoutes)
 
-const CONNECTION_URL = 'mongodb+srv://danielrubens:WWZBkyzklGRxeYyv@cluster0.qy3jhwt.mongodb.net/?retryWrites=true&w=majority'
-const PORT = process.env.PORT || 5000
+const CONNECTION_URL = process.env.CONNECTION_URL
+const PORT = process.env.PORT
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
