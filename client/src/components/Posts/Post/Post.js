c21c376c703efc5768cdb8f8fdf2a802bacd2a4d
danielrubens <danielrubensdaniel@gmail.com>
2022-11-21 16:04:23 -0300

    logic allowing only the creator to update/delete post

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -34,11 +34,13 @@ const Post = ({ post, setCurrentId }) => {
         <Typography variant='h6'>{post.name}</Typography>
         <Typography variant='body2'>{moment(post.createdAt).fromNow()}</Typography>
       </div>
-      <div className={classes.overlay2}>
-        <Button style={{color: 'white'}} size='small' onClick={() => {setCurrentId(post._id)}}>
-          <MoreHorizIcon fontSize='medium'/>
-        </Button>
-      </div>
+      {(user?.result?.googleId === post?.creator || user?.result._id === post?.creator) &&
+        <div className={classes.overlay2}>
+          <Button style={{color: 'white'}} size='small' onClick={() => {setCurrentId(post._id)}}>
+            <MoreHorizIcon fontSize='medium'/>
+          </Button>
+        </div>
+      }
       <div className={classes.details}>
         <Typography variant='body2' color='textSecondary'>{post.tags.map((tag) => `#${tag} `)}</Typography>
       </div>
@@ -47,14 +49,16 @@ const Post = ({ post, setCurrentId }) => {
         <Typography variant='body2' color='textSecondary' component='p'>{post.message}</Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
-        <Button size='small' color='primary' onClick={() => {dispatch(likePost(post._id))}}>
-          <ThumbUpAltIcon fontSize='small' />
-            &nbsp;{`Like ${post.likeCount}`}
+        <Button size='small' color='primary' disabled={!user?.result} onClick={() => {dispatch(likePost(post._id))}}>
+          <Likes />
         </Button>
-        <Button size='small' color='primary' onClick={() => {dispatch(deletePost(post._id))}}>
-          <DeleteIcon fontSize='small' />
-            Delete
-        </Button>
+       {(user?.result?.googleId === post?.creator || user?.result._id === post?.creator) &&
+          <Button size='small' color='primary' onClick={() => {dispatch(deletePost(post._id))}}>
+            <DeleteIcon fontSize='small' />
+              Delete
+          </Button>
+       }
+
       </CardActions>
     </Card>
   )
