139a0b00a17af4b7159ba5fba5b5f0b7757d8ad1
danielrubens <danielrubensdaniel@gmail.com>
2022-11-21 03:07:07 -0300

    API.interceptores.request.use

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,6 +2,14 @@ import axios from 'axios'
 
 const API = axios.create({baseURL: 'http://localhost:5000' })
 
+// it helps the auth middleware -> send token back to the backend, so the backend middleware verify user is logged in
+API.interceptors.request.use((req) => {
+    if(localStorage.getItem('profile')){
+        req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`
+    }
+    return req
+})
+
 export const fetchPosts = () => API.get('/posts')
 export const createPost = (newPost) => API.post('/posts', newPost)
 export const likePost = (id) => API.patch(`$/posts/${id}/likePost`)
